5e7809b2b9b38c863d357c2aa089ed369683863d
SahilAglawe <sahilaglawe66@gmail.com>
2025-03-05 09:36:03 +0530

    add cmt

--- Basic Maths/countDigit.java
+++ Basic Maths/countDigit.java
@@ -2,9 +2,9 @@ class Solution {
     static int evenlyDivides(int n) {
         int count = 0;
         while( n > 0 ) {
-            int lastDigit = n % 10;
+           // int lastDigit = n % 10;  when we want to extract last digit. if 7889 = 9
             count = count + 1;
-            n = n / 10;
+            n = n / 10; // 7889 = 788
         }
         return count;
     }
