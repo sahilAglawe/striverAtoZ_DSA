2a8c4900809136af62a54715653a7519ba2595ca
SahilAglawe <sahilaglawe66@gmail.com>
2025-03-16 09:48:26 +0530

    Add quick sort implementation

---
+++ sorting/quickSort.java
@@ -0,0 +1,30 @@
+class Solution {
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high]; 
+        int i = low - 1; 
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
