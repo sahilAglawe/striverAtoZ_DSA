41dcb0981934e656a71be8960b1bf0d18324bbac
SahilAglawe <sahilaglawe66@gmail.com>
2025-03-18 09:56:46 +0530

    Add implementation to rotate an array to the right by k steps

---
+++ Array/Medium/rotateArray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; // if arr size is 7 and k = 3 so remainder 4 so, we pushed 4 element on right side.
+        int[] rotated = new int[n];
+
+        for(int i = 0; i<n; i++){
+            rotated[( i + k) % n] = nums[i];
+        }
+
+        for(int i = 0; i<n; i++){
+            nums[i] = rotated[i];
+        }
+    }
+}
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
