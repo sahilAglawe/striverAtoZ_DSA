26b0f830f3026a39260fcf5b9150e5d1eb7a70f9
SahilAglawe <sahilaglawe66@gmail.com>
2025-04-15 08:26:40 +0530

    Add implementation for sorting colors

---
+++ Array/Medium/sortColors.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+    }
+}
+
+// T/C : O(NlogN)
+// S/C : O(1)
+
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
