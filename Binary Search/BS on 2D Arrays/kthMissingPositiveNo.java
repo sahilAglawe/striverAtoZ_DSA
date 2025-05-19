532db3782682a6630a5831c0bb6526a6b710e473
SahilAglawe <sahilaglawe66@gmail.com>
2025-05-19 07:58:45 +0530

    Add implementation for finding the kth missing positive number in a sorted array

---
+++ Binary Search/BS on 2D Arrays/kthMissingPositiveNo.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left <= right){
+            int mid  = left + (right - left) / 2;
+            if(arr[mid] - (mid + 1) >= k){
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left + k;
+    }
+}
+
+
+// T/C : O(logn) , S/C: O(1)
