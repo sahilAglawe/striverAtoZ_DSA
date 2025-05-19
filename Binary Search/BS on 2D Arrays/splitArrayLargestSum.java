9ce6ad27cfc87a6cdc045078310cb5498800466a
SahilAglawe <sahilaglawe66@gmail.com>
2025-05-19 08:35:22 +0530

    Add implementation for splitting an array into k subarrays with the largest sum

---
+++ Binary Search/BS on 2D Arrays/splitArrayLargestSum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int low = 0, high = 0;
+        for(int num : nums) {
+            low = Math.max(low, num);
+            high += num;
+        }
+        int ans = high;
+        while(low <= high) {
+            int mid = low + (high - low)/ 2;
+            if(isFeasible(nums, mid, k)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    private boolean isFeasible(int[] nums, int maxSum, int k){
+        int count = 1;
+        int currentSum = 0;
+        for(int num : nums) {
+            currentSum += num;
+            if(currentSum > maxSum) {
+                count++;
+                currentSum = num;
+                if(count > k) return false;
+            }
+        }
+        return true;
+    }
+}
+
+// T/C: O(nlog(sum)), S/C: O(1)
