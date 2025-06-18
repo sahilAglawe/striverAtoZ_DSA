3ae28ba6f9d938f3012831452f9f3a4d4ed2cac0
SahilAglawe <sahilaglawe66@gmail.com>
2025-06-18 11:02:50 +0530

    Add implementation for detecting a loop in a linked list

---
+++ Linked List/Solution.java
@@ -0,0 +1,21 @@
+// Question : Detect a loop in a linked list
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+// T/C: O(n)
+// S/C: O(1)
