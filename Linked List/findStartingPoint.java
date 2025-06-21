6de18eacb74ed112830fcba079662189f8f8377d
SahilAglawe <sahilaglawe66@gmail.com>
2025-06-21 12:10:34 +0530

    Add implementation for finding the starting point of a cycle in a linked list using Floyd's Cycle Detection Algorithm

---
+++ Linked List/findStartingPoint.java
@@ -0,0 +1,26 @@
+// Question : Find the starting point of a cycle in a linked list
+// Tortoise and Hare algorithm (Floyd's Cycle Detection Algorithm)
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+       ListNode slow = head, fast = head;
+
+       while(fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(slow == fast) break;
+       } 
+       if(fast == null || fast.next == null) return null;
+
+       fast = head;
+       while(fast != slow) {
+        fast = fast.next;
+        slow = slow.next;
+       }
+       return slow;
+    }
+}
+
+// T/C: O(n)
+// S/C: O(1)
