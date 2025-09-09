697e5b4b338943ca279e656b9714dfb5f9dccf64
SahilAglawe <sahilaglawe66@gmail.com>
2025-09-09 11:17:00 +0530

    Fix loop condition and divisor check in isPrime method for correct prime validation

--- Basic Maths/primeNumber.java
+++ Basic Maths/primeNumber.java
@@ -3,8 +3,8 @@
        if(n <= 1) return false;
        if(n == 2) return true;
 
-       for(int i = 2; i<Math.sqrt(i); i++) {
-          if(i % 2 == 0) {
+       for(int i = 2; i<Math.sqrt(n); i++) {
+          if(n % i == 0) {
             return false;
           }
        }
